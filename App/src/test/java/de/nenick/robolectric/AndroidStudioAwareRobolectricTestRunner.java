2f0d743f53a1f4ed7857f21141dde7378c21ea2f
Nico Küchler <nico.kuechler@epost-dev.de>
2015-03-14 21:10:21 +0100

    improve android manifest resolution for android studio

--- App/src/test/java/de/nenick/robolectric/AndroidStudioAwareRobolectricTestRunner.java
+++ App/src/test/java/de/nenick/robolectric/AndroidStudioAwareRobolectricTestRunner.java
@@ -3,16 +3,21 @@ package de.nenick.robolectric;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.RobolectricTestRunner;
 
+import java.io.File;
+
 import de.nenick.quacc.BuildConfig;
 
 public class AndroidStudioAwareRobolectricTestRunner extends RobolectricTestRunner {
 
     public AndroidStudioAwareRobolectricTestRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
-        String buildVariant = BuildConfig.BUILD_TYPE + (BuildConfig.FLAVOR.isEmpty()? "" : "/" + BuildConfig.FLAVOR);
+        String buildVariant = (BuildConfig.FLAVOR.isEmpty() ? "" : BuildConfig.FLAVOR+ "/") + BuildConfig.BUILD_TYPE;
+        String intermediatesPath = BuildConfig.class.getResource("").toString().replace("file:", "");
+        intermediatesPath = intermediatesPath.substring(0, intermediatesPath.indexOf("/classes"));
+
         System.setProperty("android.package", BuildConfig.APPLICATION_ID);
-        System.setProperty("android.manifest", "build/intermediates/manifests/full/" + buildVariant + "/AndroidManifest.xml");
-        System.setProperty("android.resources", "build/intermediates/res/" + buildVariant);
-        System.setProperty("android.assets", "build/intermediates/assets/" + buildVariant);
+        System.setProperty("android.manifest", intermediatesPath + "/manifests/full/" + buildVariant + "/AndroidManifest.xml");
+        System.setProperty("android.resources", intermediatesPath + "/res/" + buildVariant);
+        System.setProperty("android.assets", intermediatesPath + "/assets/" + buildVariant);
     }
 }
