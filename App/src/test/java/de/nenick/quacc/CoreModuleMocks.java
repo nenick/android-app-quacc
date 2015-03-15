ecaaea824efd2b157f14e2a51ad63cd579b0bdc2
Nico Küchler <nico.kuechler@epost-dev.de>
2015-03-15 23:40:30 +0100

    replace core classes with mocks for unit tests

--- App/src/test/java/de/nenick/quacc/CoreModuleMocks.java
+++ App/src/test/java/de/nenick/quacc/CoreModuleMocks.java
@@ -1,22 +1,25 @@
-package de.nenick.quacc.daggersupport;
+package de.nenick.quacc;
 
 import android.content.Context;
 
 import dagger.Module;
 import dagger.Provides;
-import de.nenick.quacc.addaccounting.AddAccountingPresenter;
 import de.nenick.quacc.addaccounting.AddAccountingPresenter_;
 import de.nenick.quacc.core.speechinterpretation.RecognizeAccountingTypeUc;
-import de.nenick.quacc.core.speechinterpretation.RecognizeAccountingTypeUc_;
+import de.nenick.quacc.daggersupport.ForApplication;
+
+import static org.mockito.Mockito.mock;
 
 @Module(
         injects = {AddAccountingPresenter_.class},
         complete = false
 )
-public class CoreModule {
+public class CoreModuleMocks {
+
+    public RecognizeAccountingTypeUc recognizeAccountingTypeUc;
 
     @Provides
     public RecognizeAccountingTypeUc provideRecognizeAccountingTypeUc(@ForApplication Context context) {
-        return RecognizeAccountingTypeUc_.getInstance_(context);
+        return recognizeAccountingTypeUc = mock(RecognizeAccountingTypeUc.class);
     }
 }
--- App/src/test/java/de/nenick/quacc/TestQuAccApplication.java
+++ App/src/test/java/de/nenick/quacc/TestQuAccApplication.java
@@ -1,4 +1,20 @@
 package de.nenick.quacc;
 
+import java.util.Arrays;
+import java.util.List;
+
+import de.nenick.quacc.daggersupport.AndroidModule;
+
 public class TestQuAccApplication extends QuAccApplication {
+
+    public static CoreModuleMocks coreModuleMocks;
+
+    @Override
+    protected List<Object> getModules() {
+        coreModuleMocks = new CoreModuleMocks();
+        return Arrays.asList(
+                new AndroidModule(this),
+                coreModuleMocks
+        );
+    }
 }
