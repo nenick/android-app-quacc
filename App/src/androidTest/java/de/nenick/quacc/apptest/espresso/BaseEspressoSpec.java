06bf3dc8bb236aba659e4c81b1f3746f565b0043
Nico Küchler <nico.kuechler@epost-dev.de>
2015-03-22 00:02:52 +0100

    try stabilize espresso tests

--- App/src/androidTest/java/de/nenick/quacc/apptest/espresso/BaseEspressoSpec.java
+++ App/src/androidTest/java/de/nenick/quacc/apptest/espresso/BaseEspressoSpec.java
@@ -1,24 +1,20 @@
 package de.nenick.quacc.apptest.espresso;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.WindowManager;
 
-import de.nenick.quacc.R;
 import de.nenick.quacc.apptest.DummyLauncherActivity_;
 import de.nenick.quacc.apptest.pages.EspressoDummyLauncherPage;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
 public class BaseEspressoSpec extends ActivityInstrumentationTestCase2<DummyLauncherActivity_> {
+    EspressoDummyLauncherPage launcherPage = new EspressoDummyLauncherPage();
+
     public BaseEspressoSpec() {
         super(DummyLauncherActivity_.class);
     }
 
-    EspressoDummyLauncherPage launcherPage = new EspressoDummyLauncherPage();
-
     public void startApp() {
-        getActivity();
+        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
         launcherPage.clickStartApp();
     }
 
