95a73d90ad5d385c1dc0f4af665e717dfaef0bdd
Nico Küchler <nico.kuechler@epost-dev.de>
2015-04-03 08:13:38 +0200

    try if overflow actionbar exist

--- App/src/androidTest/java/de/nenick/quacc/apptest/AddAccountingUcSpec.java
+++ App/src/androidTest/java/de/nenick/quacc/apptest/AddAccountingUcSpec.java
@@ -2,9 +2,16 @@ package de.nenick.quacc.apptest;
 
 import org.junit.Test;
 
+import de.nenick.quacc.R;
 import de.nenick.quacc.apptest.espresso.BaseEspressoSpec;
 import de.nenick.quacc.apptest.pages.EspressoAccountingListPage;
 import de.nenick.quacc.apptest.pages.EspressoAddAccountingPage;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class AddAccountingUcSpec extends BaseEspressoSpec {
 
@@ -24,6 +31,26 @@ public class AddAccountingUcSpec extends BaseEspressoSpec {
         addAccountingPage.chooseAccountingDate("10.11.2012");
         addAccountingPage.chooseAccountingValue("3,50");
         addAccountingPage.actionbar().clickConfirmButton();
+
+        accountingListPage.list().doesContain("Monatlich");
+    }
+
+    @Test
+    public void testActionBarOverflow() {
+
+        accountingListPage.clickAddButton();
+        addAccountingPage.isActivePage();
+        addAccountingPage.chooseAccount("Bar");
+        addAccountingPage.chooseAccountingInterval("Monatlich");
+        addAccountingPage.chooseAccountingType("Einnahme");
+        addAccountingPage.chooseAccountingCategory("Miete");
+        addAccountingPage.chooseAccountingDate("10.11.2012");
+        addAccountingPage.chooseAccountingValue("3,50");
+
+        openActionBarOverflowOrOptionsMenu(main.instrumentation().getTargetContext());
+
+        onView(withText("Fertig"))
+                .perform(click());
 
         accountingListPage.list().doesContain("Monatlich");
     }
