97e3542f19bde1f5185a89242e9bee70f3dd095c
Nico Küchler <nico.kuechler@epost-dev.de>
2015-04-06 13:18:58 +0200

    check memory usage at test run

--- App/src/test/java/de/nenick/quacc/robolectric/RoboAppTest.java
+++ App/src/test/java/de/nenick/quacc/robolectric/RoboAppTest.java
@@ -1,5 +1,6 @@
 package de.nenick.quacc.robolectric;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 
@@ -8,4 +9,11 @@ import de.nenick.quacc.BuildConfig;
 @RunWith(AndroidStudioAwareRobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public abstract class RoboAppTest {
+
+    @Before
+    public void checkMemory() {
+        // Total amount of free memory available to the JVM
+        System.out.println("Free memory (bytes): " +
+                Runtime.getRuntime().freeMemory());
+    }
 }
