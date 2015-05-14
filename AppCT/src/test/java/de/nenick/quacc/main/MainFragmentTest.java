1016b2b8a47b5ab96456f92cc35111285b3c39c0
nenick <kuchler.nico@gmail.com>
2015-05-14 19:31:12 +0200

    fix build

--- AppCT/src/test/java/de/nenick/quacc/main/MainFragmentTest.java
+++ AppCT/src/test/java/de/nenick/quacc/main/MainFragmentTest.java
@@ -3,12 +3,12 @@ package de.nenick.quacc.main;
 import org.junit.Test;
 
 import de.nenick.quacc.accounting.list.RoboAccountingListPage;
-import de.nenick.robolectric.RoboAppTest;
+import de.nenick.robolectric.RoboComponentTestBase;
 import de.nenick.robolectric.RoboSup;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class MainFragmentTest extends RoboAppTest {
+public class MainFragmentTest extends RoboComponentTestBase {
 
     RoboSup<MainActivity_, MainFragment_> robo = new RoboSup<>();
     RoboMainPage mainPage = new RoboMainPage(robo);
