39d0df28d52a46e73ef87accc8e425ebed9365de
nenick <kuchler.nico@gmail.com>
2015-06-13 12:25:30 +0200

    fix compile error after last changes

--- App/src/main/java/de/nenick/quacc/accounting/list/NavigationDrawerFragment.java
+++ App/src/main/java/de/nenick/quacc/accounting/list/NavigationDrawerFragment.java
@@ -237,24 +237,10 @@ public class NavigationDrawerFragment extends Fragment {
         // If the drawer is open, show the global app actions in the action bar. See also
         // showGlobalContextActionBar, which controls the top-left area of the action bar.
         if (mDrawerLayout != null && isDrawerOpen()) {
-            inflater.inflate(R.menu.global, menu);
+            inflater.inflate(R.menu.menu_main, menu);
             showGlobalContextActionBar();
         }
         super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
