a63915bdadb66df8715647f31ed72c54e695b37f
nenick <kuchler.nico@gmail.com>
2015-06-28 16:51:45 +0200

    adjust tests after last changes

--- App/src/androidTest/java/de/nenick/quacc/apptest/pages/EspressoAccountingList.java
+++ App/src/androidTest/java/de/nenick/quacc/apptest/pages/EspressoAccountingList.java
@@ -1,43 +1,5 @@
 package de.nenick.quacc.apptest.pages;
 
-import android.support.test.espresso.matcher.BoundedMatcher;
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-
-import java.util.Map;
-
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.internal.util.Checks.checkNotNull;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.core.IsEqual.equalTo;
-
 public class EspressoAccountingList {
-    public void doNotContain(String text) {
 
-    }
-
-    public void doesContain(String text) {
-        /*onData(withItemContent(text))
-                .onChildView(withId(R.id.list_item))
-                .check(matches(isDisplayed()));
-                */
-    }
-
-    public static Matcher<Object> withItemContent(String text) {
-        checkNotNull(text);
-        final Matcher<String> itemTextMatcher = equalTo(text);
-        return new BoundedMatcher<Object, Map>(Map.class) {
-            @Override
-            public boolean matchesSafely(Map map) {
-                return hasEntry(equalTo("description"), itemTextMatcher).matches(map);
-            }
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("with item content: ");
-                itemTextMatcher.describeTo(description);
-            }
-        };
-    }
 }
--- App/src/androidTest/java/de/nenick/quacc/apptest/pages/EspressoAddAccountingPage.java
+++ App/src/androidTest/java/de/nenick/quacc/apptest/pages/EspressoAddAccountingPage.java
@@ -1,27 +1,38 @@
 package de.nenick.quacc.apptest.pages;
 
+import android.database.sqlite.SQLiteCursor;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.DatePicker;
 
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 
 import de.nenick.quacc.R;
+import de.nenick.quacc.database.provider.category.CategoryCursor;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.internal.util.Checks.checkNotNull;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class EspressoAddAccountingPage {
 
@@ -50,7 +61,34 @@ public class EspressoAddAccountingPage {
 
     public void chooseAccountingCategory(final String text) {
         onView(withId(R.id.category)).perform(click());
-        onData(allOf(is(instanceOf(String.class)), is(text))).perform(click());
+        onData(allOf(is(instanceOf(CategoryCursor.class)), withItemContent(text))).perform(click());
+    }
+
+    public static Matcher<Object> withItemContent(String expectedText) {
+        // use preconditions to fail fast when a test is creating an invalid
+        // matcher.
+        checkNotNull(expectedText);
+        return withItemContent(equalTo(expectedText));
+    }
+
+    public static Matcher<Object> withItemContent(final Matcher<String> itemTextMatcher) {
+        // use preconditions to fail fast when a test is creating an invalid
+        // matcher.
+        checkNotNull(itemTextMatcher);
+        return new BoundedMatcher<Object, CategoryCursor>(
+                CategoryCursor.class) {
+
+            @Override
+            public void describeTo(Description description) {
+                itemTextMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(CategoryCursor arg0) {
+                return itemTextMatcher
+                        .matches(arg0.getName());
+            }
+        };
     }
 
     public void chooseAccountingValue(String text) {
