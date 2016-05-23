2bda04cc88761628319c9bf33c11b1aeb2feea04
Nico Küchler <nico.kuechler@epost-dev.de>
2016-05-23 10:16:15 +0200

    document meanings

--- app/src/main/java/de/nenick/quacc/view/accounting_overview/filter/FilterRange.java
+++ app/src/main/java/de/nenick/quacc/view/accounting_overview/filter/FilterRange.java
@@ -2,6 +2,19 @@ package de.nenick.quacc.view.accounting_overview.filter;
 
 public enum  FilterRange {
 
-    all_accounting, current_month, free
+    /**
+     * User will see all booking entries done until now.
+     */
+    all_accounting,
 
+    /**
+     * User see only booking entries from current month.
+     * TODO maybe obs
+     */
+    current_month,
+
+    /**
+     * User may select his prefered range
+     */
+    free
 }
